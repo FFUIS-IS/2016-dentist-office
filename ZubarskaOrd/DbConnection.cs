c5ca2f3bc949e0474f3672bac4ffe3d83a0cb8ea
thomasinjo <tomoklacar@gmail.com>
2017-01-26 11:52:37 +0100

    .

--- ZubarskaOrd/DbConnection.cs
+++ ZubarskaOrd/DbConnection.cs
@@ -13,15 +13,12 @@ namespace ZubarskaOrd
 
         private DbConnection()
         {
-            MessageBox.Show(Program.path);
             string connectionString = "Data Source=" + Program.path + "Database.sdf; Password=database32";
-
             connection = new SqlCeConnection(connectionString);
             try { connection.Open();  }
             catch (Exception e)
             { MessageBox.Show(e.ToString()); };
-            
-            
+
         }
 
         public static DbConnection Instance
